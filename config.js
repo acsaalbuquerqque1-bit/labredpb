b6be294ffc62810b9441dcff53036a81fa7ccb03
acsaalbuquerqque1-bit <acsaalbuquerqque1@gmail.com>
2026-08-24 14:45:34 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Configuração pública do Supabase para o site.
+window.SUPABASE_URL = 'https://motvcavziwprbstorqbw.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_dCSCfYBA5X80LzAi1Q6S1w_ZyZ0ZelG';
